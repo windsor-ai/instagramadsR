09bcb10506d87412f5aa09b33b30ca16025d0451
Pablo Sanchez-Martinez <p.sanchez@creaf.uab.cat>
2022-10-10 19:08:01 +0200

    Function and data.

---
+++ R/my_instagramads_data.R
@@ -0,0 +1,15 @@
+#' Sample data of Facebook ads from the Windsor API.
+#'
+#' A dataset containing sample facebook ads data
+#' fetched from windsor.ai API. See more at:  https://windsor.ai/
+#'
+#' @format A data frame with 164 rows and 5 variables:
+#' \describe{
+#'   \item{campaign}{name of the campaign}
+#'   \item{clicks}{number of clicks}
+#'   \item{spend}{spend data}
+#'   \item{impressions}{impressions data}
+#'   \item{date}{date}
+#' }
+#' @source \url{https://windsor.ai/}
+"my_facebookads_data"
